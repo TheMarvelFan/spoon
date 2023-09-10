0007fec0e4a68bb7c187528514ad821eda05cd0d
SirYwell <hannesgreule@outlook.de>
2023-09-10 21:30:28 +0200

    Work

--- src/main/java/spoon/reflect/visitor/DefaultJavaPrettyPrinter.java
+++ src/main/java/spoon/reflect/visitor/DefaultJavaPrettyPrinter.java
@@ -2336,6 +2336,9 @@ public class DefaultJavaPrettyPrinter implements CtVisitor, PrettyPrinter {
 
 	@Override
 	public void visitCtCasePattern(CtCasePattern casePattern) {
+		scan(casePattern.getPattern());
+		printer.writeSpace().writeKeyword("when").writeSpace();
+		scan(casePattern.getGuard());
 		// FIXME or not
 	}
 }
